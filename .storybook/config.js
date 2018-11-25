e45c41e6931c64a9b199faf8e8d01dd01350181b
Eddie Padin <eddie@entertainermedia.net>
2018-11-25 12:19:22 -0600

    core: add storybook support; - add autoload support to storybook config;

--- .storybook/config.js
+++ .storybook/config.js
@@ -1,7 +1,12 @@
-import { configure } from '@storybook/react';
+import React from "react";
+import { configure } from "@storybook/react";
+
+// dynamically load in all the stories in the /stories directory
+// https://github.com/storybooks/storybook/issues/125#issuecomment-212404756
+const req = require.context("../stories/", true, /story\.js$/);
 
 function loadStories() {
-  require('../stories');
+  req.keys().forEach(req);
 }
 
 configure(loadStories, module);
