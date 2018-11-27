58d7b310319207b6e798644e03d23cf3c7714c50
Eddie Padin <eddie@entertainermedia.net>
2018-11-27 17:51:40 -0600

    style_linting

--- .storybook/config.js
+++ .storybook/config.js
@@ -3,7 +3,7 @@ import { configure } from "@storybook/react";
 
 // dynamically load in all the stories in the /stories directory
 // https://github.com/storybooks/storybook/issues/125#issuecomment-212404756
-const req = require.context("../stories/", true, /story\.js$/);
+const req = require.context("../stories/", true, /\.story\.js$/);
 
 function loadStories() {
   req.keys().forEach(req);
