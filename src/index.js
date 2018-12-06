eaa2a15c4c845efc2466e93578cc433ee2a107b3
Eddie Padin <eddie@entertainermedia.net>
2018-12-05 19:18:24 -0600

    dependency: add redux-dev-tools-extension;

--- src/index.js
+++ src/index.js
@@ -6,8 +6,9 @@ import { createStore } from "redux";
 import { Provider } from "react-redux";
 import reducer from "./reducers";
 import middleware from "./middleware";
+import { composeWithDevTools } from "redux-devtools-extension";
 
-const store = createStore(reducer, middleware);
+const store = createStore(reducer, composeWithDevTools(middleware));
 
 ReactDOM.render(
   <Provider store={store}>
