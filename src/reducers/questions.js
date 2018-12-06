d409a92754d520dcb346c48fec3f569d92df9ca8
Eddie Padin <eddie@entertainermedia.net>
2018-12-06 01:30:34 -0600

    feature: voting functionality; - add logic to reducer that restricts a user from voting for the same question multiple times;

--- src/reducers/questions.js
+++ src/reducers/questions.js
@@ -16,24 +16,37 @@ const questions = (state = {}, action) => {
         ...action.questions
       };
     case VOTE_QUESTION_ANSWER:
+      //get the users that have already answered the question as an array from the prev state
+      const usersAnswered = [...state[action.questionId][action.answer].votes];
+      //check if the current user has answered the question already
+      const userHasAnswered = usersAnswered.includes(action.authUser);
+      //create an empty object to use later
+      let setUserVote = {};
+      //if user has NOT answered the question already
+      if (!userHasAnswered) {
+        //create a new object to pass to the store
+        setUserVote = {
+          //get the question id & build the new question object
+          [action.questionId]: {
+            //spread the state from the prev question object
+            ...state[action.questionId],
+            //find the answer object
+            [action.answer]: {
+              //spread the  state from the prev answer object
+              ...state[action.questionId][action.answer],
+              //update the votes on the answer object with the new state
+              votes:
+                //add the user's vote
+                state[action.questionId][action.answer].votes.concat([
+                  action.authUser
+                ])
+            }
+          }
+        };
+      }
       return {
         ...state,
-        //get the question id & build the new object
-        [action.questionId]: {
-          //spread the state from the old object
-          ...state[action.questionId],
-          //find the answer object
-          [action.answer]: {
-            //spread the  state from the old answer object
-            ...state[action.questionId][action.answer],
-            //update the votes on the answer object with the new state
-            votes:
-              //if the user already voted, than do nothing,  otherwise add their vote
-              state[action.questionId][action.answer].votes.concat([
-                action.authUser
-              ])
-          }
-        }
+        ...setUserVote
       };
     default:
       return state;
