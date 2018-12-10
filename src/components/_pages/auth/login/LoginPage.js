0184625646db0da9c6f9285be03e48ccd961b6f7
Eddie Padin <eddie@entertainermedia.net>
2018-12-10 15:17:40 -0600

    feat: routing; - add login page (WIP);

---
+++ src/components/_pages/auth/login/LoginPage.js
@@ -0,0 +1,13 @@
+import React, { Component } from "react";
+
+class LoginPage extends Component {
+  render() {
+    return (
+      <div>
+        <h1>This is the login page</h1>
+      </div>
+    );
+  }
+}
+
+export default LoginPage;
